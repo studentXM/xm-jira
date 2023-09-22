4943210433a951f4d0b6d9189ebbc99b051fa89b
xinmu <2953282238@qq.com>
2023-09-22 17:03:58 +0800

    r

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import appStyles from "./App.scss";
 import a from "@/store/index";
 console.log(a);
+
 function App() {
   return <div className={appStyles.box}>hello everyone</div>;
 }
