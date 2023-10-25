224fbf31ec4e1c80ba14333b8a95d3a83b59cbac
xinmu <2953282238@qq.com>
2023-10-25 18:36:36 +0800

    antd Form

--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -1,13 +1,13 @@
 // 事件中的参数类型
 import { useAuth } from "@/context/auth-context"
-import { Button, Form } from "antd"
-import { Input } from "postcss"
+import { Button, Form, Input } from "antd"
 import { FormEvent } from "react"
 
-export const LoginScreen = () => {
+export const LoginScreen: React.FC = () => {
     console.log(useAuth())
     const { login, user } = useAuth()
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        console.log(event)
         event.preventDefault()
         const username = (event.currentTarget.elements[0] as HTMLInputElement)
             .value
@@ -16,14 +16,18 @@ export const LoginScreen = () => {
         login({ username, password })
     }
     return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Item name={"username"}>
-                <Input placeholder="用户名" type="text" />
-            </Form.Item>
-            <Form.Item name={"password"}>
-                <Input placeholder="密码" type="password" />
-            </Form.Item>
-            <Button type={"submit"}>登录</Button>
-        </Form>
+        <div>
+            <Form onFinish={handleSubmit}>
+                <Form.Item name="username">
+                    <Input placeholder="用户名" type="text" />
+                </Form.Item>
+                <Form.Item name="password">
+                    <Input placeholder="密码" type="password" />
+                </Form.Item>
+                <Form.Item>
+                    <Button htmlType="submit">登录</Button>
+                </Form.Item>
+            </Form>
+        </div>
     )
 }
