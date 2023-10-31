41ddee1f2857d869a4e913bf5a14dfc35b5c71dc
xinmu <2953282238@qq.com>
2023-10-31 18:32:11 +0800

    antd

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -4,6 +4,8 @@ import { ProjectListScreen } from "./screens/project-list"
 // 登陆状态
 export const AuthenticatedApp = () => {
     const { logOut } = useAuth()
+    console.log(process.env.NODE_ENV)
+
     return (
         <div>
             <button onClick={logOut}>登出</button>
